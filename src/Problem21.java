84ba06599d54125a4383151bbfbb5e74968bcad8
Dmitriy Panasiuk <dmitriy.panasyuk@gmail.com>
2015-01-02 00:47:13 +0200

    Problem 21 solved

--- src/Problem21.java
+++ src/Problem21.java
@@ -1,8 +1,8 @@
 /*Let d(n) be defined as the sum of proper divisors of n (numbers less than n which divide evenly into n).
-        If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called amicable numbers.
-
-        For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284. The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
-
+        If d(a) = b and d(b) = a, where a ≠ b, then a and b are an amicable pair and each of a and b are called
+        amicable numbers.
+        For example, the proper divisors of 220 are 1, 2, 4, 5, 10, 11, 20, 22, 44, 55 and 110; therefore d(220) = 284.
+        The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
         Evaluate the sum of all the amicable numbers under 10000.*/
 
 public class Problem21 {
@@ -24,7 +24,7 @@ public class Problem21 {
     private static int sum(int[] numbers) {
         int sum = 0;
         for (int i = 1; i < numbers.length; i++) {
-            if (numbers[i] < N && numbers[i] == numbers[numbers[i]] && numbers[i] != i) {
+            if (numbers[i] < N && i == numbers[numbers[i]] && numbers[i] != i && i < numbers[i]) {
                 sum += i + numbers[i];
             }
         }
