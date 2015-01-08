151e6084e9fd8e8544a5801842f216c6f64e1aa8
global\dpanasiuk <dpanasiuk@sdl.com>
2015-01-08 15:29:39 +0200

    Added rational class

--- src/InterviewTask.java
+++ src/InterviewTask.java
@@ -7,7 +7,50 @@ import java.util.List;
 public class InterviewTask {
     public static void main(String[] args) {
         List<Character> numbers = Arrays.asList('1', '2', '3');
-        System.out.println(Combinator.getPermutations(numbers));
-        System.out.println(Combinator.getCombinations(numbers, 1));
+        Rational r = new Rational(8,-2);
+        System.out.println(r);
+    }
+}
+
+class Rational {
+    private int numerator;
+    private int denominator;
+
+    Rational(int numerator) {
+        this(numerator, 1);
+    }
+
+    Rational(int numerator, int denominator) {
+        if(denominator == 0) throw new IllegalArgumentException("denominator can't be zero!");
+        this.numerator = numerator;
+        this.denominator = denominator;
+        normalize();
+    }
+
+    private void normalize() {
+        if (denominator < 0) {
+            numerator *= -1;
+            denominator *= -1;
+        }
+        int divisor = 2;
+        while (Math.abs(numerator) != 1 && denominator != 1 && divisor <= Math.abs(numerator) && divisor <= denominator) {
+            if (numerator % divisor == 0 && denominator % divisor == 0) {
+                numerator /= divisor;
+                denominator /= divisor;
+                divisor = 2;
+                continue;
+            }
+            divisor++;
+        }
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer();
+        sb.append(numerator);
+        if (denominator != 1) {
+            sb.append("/").append(denominator);
+        }
+        return sb.toString();
     }
 }
