2641aadf5ebbe289690d49a7f58d59d77d4b3de4
global\dpanasiuk <dpanasiuk@sdl.com>
2015-01-08 15:10:08 +0200

    Added combinations implementation

--- src/Helpers/Combinator.java
+++ src/Helpers/Combinator.java
@@ -1,8 +1,6 @@
 package Helpers;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 public class Combinator {
@@ -10,6 +8,12 @@ public class Combinator {
         List<List<E>> permutations = new ArrayList<List<E>>();
         getPermutations(initial, permutations, new boolean[initial.size()], new ArrayList<E>(initial), 0);
         return permutations;
+    }
+
+    public static <E> List<List<E>> getCombinations(List<E> initial, int k) {
+        List<List<E>> combinations = new ArrayList<List<E>>();
+        getCombinations(initial, combinations, new ArrayList<E>(initial.subList(0, k)), 0);
+        return combinations;
     }
 
     private static <E> void getPermutations(List<E> chars, List<List<E>> permutations, boolean[] used, List<E> current, int level) {
@@ -24,6 +28,17 @@ public class Combinator {
             used[i] = true;
             getPermutations(chars, permutations, used, current, level + 1);
             used[i] = false;
+        }
+    }
+
+    private static <E> void getCombinations(List<E> chars, List<List<E>> combinations, List<E> current, int level) {
+        if (level == current.size()) {
+            combinations.add(new ArrayList<E>(current));
+            return;
+        }
+        for (int i = 0; i < chars.size(); i++) {
+            current.set(level, chars.get(i));
+            getCombinations(chars, combinations, current, level + 1);
         }
     }
 }
--- src/InterviewTask.java
+++ src/InterviewTask.java
@@ -1,14 +1,13 @@
+/*We have numbers 1, 3, 4 and 6. Create an expression using any elementary operators (+, -, *, /),
+containing only these numbers, exactly once each, such that the result is 24.*/
 import Helpers.Combinator;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-/*We have numbers 1, 3, 4 and 6. Create an expression using any elementary operators (+, -, *, /),
-containing only these numbers, exactly once each, such that the result is 24.*/
 public class InterviewTask {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 3, 4);
+        List<Character> numbers = Arrays.asList('1', '2', '3');
         System.out.println(Combinator.getPermutations(numbers));
+        System.out.println(Combinator.getCombinations(numbers, 1));
     }
 }
