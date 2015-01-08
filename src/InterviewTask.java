65b78762e5e91b1585d16550dc21c7ef777dc5a4
global\dpanasiuk <dpanasiuk@sdl.com>
2015-01-08 16:48:15 +0200

    Added Reverse Polish Notation implementation

--- src/InterviewTask.java
+++ src/InterviewTask.java
@@ -1,18 +1,16 @@
 /*We have numbers 1, 3, 4 and 6. Create an expression using any elementary operators (+, -, *, /),
 containing only these numbers, exactly once each, such that the result is 24.*/
 import Helpers.Combinator;
-import java.util.Arrays;
-import java.util.List;
+
+import java.util.*;
 
 public class InterviewTask {
     public static void main(String[] args) {
-        List<Character> numbers = Arrays.asList('1', '2', '3');
+        List<Character> numbers = Arrays.asList('1', '3', '3');
         Rational r = new Rational(2);
-        System.out.println(r.add(new Rational(3)));
-        System.out.println(r.subtract(new Rational(3)));
-        System.out.println(r.multiply(new Rational(3)));
-        System.out.println(r.divide(new Rational(1)));
-
+        ReversePolishNotation example = new ReversePolishNotation(Arrays.asList(new Rational(1), new Rational(2), '+', new Rational(3), '*'));
+        System.out.println(example);
+        System.out.println(example.evaluate());
     }
 }
 
@@ -75,6 +73,80 @@ class Rational {
         if (denominator != 1) {
             sb.append("/").append(denominator);
         }
+        return sb.toString();
+    }
+}
+
+class ReversePolishNotation {
+    Deque<Object> expression;
+    ReversePolishNotation(List<Object> expression) {
+        this.expression = new ArrayDeque<Object>(expression);
+    }
+
+    public Rational evaluate() {
+        Deque<Rational> stack = new ArrayDeque<Rational>();
+        Deque<Object> copyOfExpression = new ArrayDeque<Object>(expression);
+        Object nextElement;
+        Rational firstOperand, secondOperand;
+        char operator;
+
+        while (!copyOfExpression.isEmpty()) {
+            nextElement = copyOfExpression.pop();
+            if (nextElement instanceof Rational) {
+                stack.addFirst((Rational)nextElement);
+            } else {
+                operator = (Character)nextElement;
+                firstOperand = stack.pop();
+                secondOperand = stack.pop();
+                switch (operator) {
+                    case '+':
+                        stack.addFirst(secondOperand.add(firstOperand));
+                        break;
+                    case '-':
+                        stack.addFirst(secondOperand.subtract(firstOperand));
+                        break;
+                    case '*':
+                        stack.addFirst(secondOperand.multiply(firstOperand));
+                        break;
+                    case '/':
+                        stack.addFirst(secondOperand.divide(firstOperand));
+                        break;
+                    default:
+                        throw new IllegalArgumentException("No such operation " + operator);
+                }
+            }
+        }
+        if (stack.size() > 1) throw new IllegalStateException("Invalid state of expression in the end of evaluation" + stack);
+
+        return stack.pop();
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        Deque<Rational> stack = new ArrayDeque<Rational>();
+        Deque<Object> copyOfExpression = new ArrayDeque<Object>(expression);
+        Object nextElement;
+        Rational firstOperand, secondOperand;
+        char operator;
+
+        //sb.append(copyOfExpression.pop());
+        while (!copyOfExpression.isEmpty()) {
+            nextElement = copyOfExpression.pop();
+            if (nextElement instanceof Rational) {
+                stack.addFirst((Rational)nextElement);
+            } else {
+                operator = (Character)nextElement;
+                firstOperand = stack.pop();
+                if (!stack.isEmpty()) {
+                    secondOperand = stack.pop();
+                    sb = sb.append('(').append(secondOperand).append(operator).append(firstOperand).append(')');
+                } else {
+                    sb = sb.insert(0, '(').append(operator).append(firstOperand).append(')');
+                }
+            }
+        }
+        if (stack.size() > 1) throw new IllegalStateException("Invalid state of expression in the end of evaluation" + stack);
         return sb.toString();
     }
 }
