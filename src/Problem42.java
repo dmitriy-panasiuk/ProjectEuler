b92caf926ca68b63ddaaf3ff4ef02d6754ff1bbf
DPanasiuk <dmitriy.panasyuk@gmail.com>
2015-05-21 22:01:58 +0300

    Problem 42 solved

--- src/Problem42.java
+++ src/Problem42.java
@@ -1,3 +1,7 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /*
 The nth term of the sequence of triangle numbers is given by, tn = ?n(n+1); so the first ten triangle numbers are:
 
@@ -11,5 +15,40 @@ Using words.txt (right click and 'Save Link/Target As...'), a 16K text file cont
  English words, how many are triangle words?
 */
 public class Problem42 {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(System.getProperty("user.dir") + "\\resources\\problem42.txt"));
+        scanner.useDelimiter(",");
+        String str;
+        int value, numberOfTriangles = 0;
+        while (scanner.hasNext()) {
+            str = scanner.next().replaceAll("\"", "");
+            value = wordValue(str);
+            if (isTriangle(value)) {
+                //System.out.println(str + " value is " + value);
+                numberOfTriangles++;
+            }
+        }
+        System.out.println(numberOfTriangles);
+    }
 
+    private static int wordValue(String word) {
+        int value = 0;
+        for (int i = 0; i < word.length(); i++) {
+            value += word.charAt(i)- 64;
+        }
+        return value;
+    }
+
+    private static boolean isTriangle(int n) {
+        return isPerfectSquare(8 * n + 1);
+    }
+
+    public static boolean isPerfectSquare(long n)
+    {
+        if (n < 0)
+            return false;
+
+        long tst = (long)(Math.sqrt(n) + 0.5);
+        return tst*tst == n;
+    }
 }
