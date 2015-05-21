ff17d20e17f1ee1f498ccc340a6173069e510d31
global\dpanasiuk <dpanasiuk@sdl.com>
2015-05-21 14:50:27 +0300

    Problem 40 solved

--- src/Problem40.java
+++ src/Problem40.java
@@ -1,9 +1,19 @@
+/*An irrational decimal fraction is created by concatenating the positive integers:
+
+        0.123456789101112131415161718192021...
+
+        It can be seen that the 12th digit of the fractional part is 1.
+
+        If dn represents the nth digit of the fractional part, find the value of the following expression.
+
+        d1 ? d10 ? d100 ? d1000 ? d10000 ? d100000 ? d1000000
+*/
 public class Problem40 {
     public static void main(String[] args) {
         int n = 1;
         int digit = 0;
         String s = "";
-        while (n < 100000) {
+        while (n < 200000) {
             s += n;
             n++;
         }
