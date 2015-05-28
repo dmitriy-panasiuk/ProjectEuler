6fb8fbc478888b2d78af3f23921f612a27f7b0e7
global\dpanasiuk <dpanasiuk@sdl.com>
2015-05-28 17:58:34 +0300

    Problem 50 solved

--- src/Problem50.java
+++ src/Problem50.java
@@ -14,25 +14,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Problem50 {
+    private static final int TRESHOLD = 1000000;
+
     public static void main(String[] args) {
-        int sum = 0, primeDiff, distance = 0, maxPrime = 0;
-        Seed seed = new Seed(200);
+        int sum = 0, primeDiff, distance = 0, maxPrime = 0, startIndex = 0, endIndex = 0;
+        Seed seed = new Seed(10000);
         List<Integer> primeSums = new ArrayList<>();
+        List<Integer> primes = seed.getPrimes_list();
         primeSums.add(sum);
-        for (int prime : seed.getPrimes_list()) {
+        for (int prime : primes) {
             sum += prime;
             primeSums.add(sum);
         }
 
         for (int i = 0; i < primeSums.size(); i++) {
-            if (primeSums.get(i) > 1000) {
+            if (primeSums.get(i) > TRESHOLD) {
                 break;
             }
             for (int j = primeSums.size() - 1; j > i + distance; j--) {
                 primeDiff = primeSums.get(j) - primeSums.get(i);
-                if (primeDiff < 1000 && isPrime(primeDiff)) {
+                if (primeDiff < TRESHOLD && isPrime(primeDiff)) {
                     if (distance < j - i) {
                         distance = j - i;
+                        startIndex = i; endIndex = j;
                         maxPrime = primeDiff;
                         break;
                     }
@@ -40,12 +44,15 @@ public class Problem50 {
             }
         }
         System.out.println(maxPrime + " " + distance);
+        for (int i = startIndex; i < endIndex; i++) {
+            System.out.print(primes.get(i) + " ");
+        }
     }
     private static boolean isPrime(int n) {
         if (n % 2 == 0 || n % 3 == 0) {
             return false;
         }
-        for (int i = 5; i * i < n; i+= 2) {
+        for (int i = 5; i * i <= n; i+= 2) {
             if (n % i == 0) {
                 return false;
             }
